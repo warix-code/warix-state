405cc0c99bd8df95de6803e7a03ed121619a0da4
Rogelio <warixcode@gmail.com>
2020-02-15 22:08:45 -0600

    Deleted build files

--- index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './interfaces';
-export { WarixState } from './warix.state';
-export * from './warix.state-action-type';
-export * from './warix.state-actions';
-export * from './warix.state-proxy';
-export * from './warix.state-utils';
--- warix.state-utils.d.ts
+++
@@ -1,5 +0,0 @@
-import { IWarixStateReducerResult } from './include';
-export declare const isReducerResult: <X>(value: any) => value is IWarixStateReducerResult<X>;
-export declare const ensureArray: (v: string | string[]) => string[];
-export declare const combinePaths: (a: string | string[], b: string | string[]) => string[];
-export declare const resolvePath: (path: string[]) => string[];
